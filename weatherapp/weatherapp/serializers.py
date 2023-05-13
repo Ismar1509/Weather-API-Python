c14478bcd1e870f61959bdc9de92e1824ea0533b
Ismar1509 <ismaraganovic69@gmail.com>
2023-05-13 02:40:19 +0200

    Reorganization of controllers, add error handling

--- weatherapp/weatherapp/serializers.py
+++ weatherapp/weatherapp/serializers.py
@@ -5,4 +5,10 @@ class CurrentWeatherRequestSerializer(serializers.Serializer):
 
 class ForecastWeatherRequestSerializer(serializers.Serializer):
     location = serializers.CharField(required=True, max_length=100)
-    #days = serializers.IntegerField(required=False, min_value=1, max_value=7)
+    days = serializers.IntegerField(required=True, min_value=1, max_value=7)
+
+class HistoryWeatherRequestSerializer(serializers.Serializer):
+    location = serializers.CharField(required=True, help_text='City name and country code, e.g. London,UK')
+    start_date = serializers.DateField(required=True, help_text='Starting date in YYYY-MM-DD format')
+    end_date = serializers.DateField(required=True, help_text='Ending date in YYYY-MM-DD format')
+
